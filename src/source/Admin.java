b09efe4b69be7b3b44314fe93e8dba97a048fefa
Aleksandar Petrovic <aca.petrovic1998.ap@gmail.com>
2022-02-25 13:46:54 +0100

    Update strukture projekta

--- src/source/Admin.java
+++ src/source/Admin.java
@@ -1,5 +1,24 @@
 package source;
 
 public class Admin {
+	private final String name;
+	private final String surname;
+	private final String username;
+	private final String password;
+	private final Role role;
+	
+	public Admin(String name, String surname, String username, String password) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.username = username;
+		this.password = password;
+		this.role = Role.ADMIN;
+	}
+	
 
+	
+	
+	
+	
 }
--- src/source/Editor.java
+++ src/source/Editor.java
@@ -1,5 +1,43 @@
 package source;
 
-public class Editor {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Editor {
+	static List<Editor> editors = new ArrayList<>();
+	private final String name;
+	private final String surname;
+	private final String username;
+	private final String password;
+	private final Role role;
+	
+	
+	
+	public Editor(String name, String surname, String username, String password) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.username = username;
+		this.password = password;
+		this.role = Role.EDITOR;
+	}
+	
+	public void add(Editor editor) {
+		editors.add(editor);
+	}
+	
+	public void print() {
+	
+	}
+	public void printAll() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+
+	public List<Editor> getEditors(){
+		return editors;
+	}
 }
